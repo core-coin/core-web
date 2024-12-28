ba4daabdafc4a0574c48d97eeb126dd069efb130
Omid Nikrah <omidnikrah@gmail.com>
2024-12-28 13:47:17 +0330

    💄 style: fix style of TOC in the documentation pages

--- src/components/Docs/PageSidebar.tsx
+++ src/components/Docs/PageSidebar.tsx
@@ -38,14 +38,15 @@ const TOC = ({
           "relative block border-l-2 border-l-[#222] py-4 pl-[26px] font-regular",
           "before:absolute before:left-[-2px] before:top-0 before:h-full before:w-[2px] before:bg-[linear-gradient(180deg,rgba(255,255,255,0.1)_0%,rgba(0,255,111,0.5)_48.64%,rgba(255,255,255,0.1)_100%)] before:opacity-0 before:transition-opacity before:duration-300 after:absolute after:left-[-5px] after:top-1/2 after:size-2 after:-translate-y-1/2 after:rounded-full after:border after:border-brand after:bg-black after:opacity-0 after:transition-opacity after:duration-300",
           {
-            "before:opacity-100 after:opacity-100": activeId === item.slug,
+            "before:opacity-100 after:opacity-100 text-brand":
+              activeId === item.slug,
           },
         )}
       >
         {item.text}
       </a>
       {item.children.length > 0 && (
-        <ul className="ml-4 text-sm">
+        <ul className="text-sm [&_a]:pl-10">
           <TOC items={item.children} />
         </ul>
       )}
