112e5fefe840b9259729f264e9db63af00cde851
Omid Nikrah <omidnikrah@gmail.com>
2025-01-16 16:20:53 +0330

    🐛 fix: mute assets videos

--- src/components/AssetsVideoLoader/index.tsx
+++ src/components/AssetsVideoLoader/index.tsx
@@ -10,11 +10,12 @@ export const AssetsVideoLoader = ({
   className,
 }: IAssetsVideoLoaderProps) => (
   <RenderOnViewport>
-    {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
     <video
       src={src}
       className={className}
       controls={false}
+      playsInline
+      muted
       autoPlay
       loop
     ></video>
